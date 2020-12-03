16e9963d76dc07a706d2f34020df6eaca56cc720
Sarah Edwards <skedwards88@gmail.com>
2020-12-02 19:58:04 -0800

    Update run.py

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ print("working")
 # import os
 # unm = os.environ['USERNAME']
 # print(unm)
-filename = "test.txt"
+filename = "mydir/test.txt"
 with open(filename, 'w') as file:
   file.write('hi')
 print('test output')
