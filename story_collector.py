21af71aa1a016b2f39ce55599c0fe4a3c0d10a8e
Sarah Edwards <skedwards88@gmail.com>
2020-12-02 20:08:15 -0800

    Update and rename run.py to story_collector.py

---
+++ story_collector.py
@@ -0,0 +1,40 @@
+# Get all posts in r/writingprompts by username
+
+import requests
+import requests.auth
+import os
+
+# Get access token (expires after 1 hour)
+client_auth = requests.auth.HTTPBasicAuth(os.environ['REDDIT_CLIENT_ID'], os.environ['REDDIT_SECRET'])
+post_data = {"grant_type": "password", "username": os.environ['REDDIT_USERNAME'], "password": os.environ['REDDIT_PASSWORD']}
+headers = {"User-Agent": "ChangeMeClient/0.1 by YourUsername"}
+response = requests.post("https://www.reddit.com/api/v1/access_token", auth=client_auth, data=post_data, headers=headers)
+access_token = response.json()['access_token']
+
+# Get all top level comments for specified user in the specified subreddit, write each to a file
+user = os.environ['REDDIT_COLIN_USERNAME']
+subreddit = 'WritingPrompts'
+output_directory = 'raw_stories'
+headers = {"Authorization": f"bearer {access_token}", "User-Agent": "ChangeMeClient/0.1 by YourUsername"}
+count = 0
+after = 'None'
+limit = 100 # This is the max
+num_comments = limit
+while num_comments == limit:
+    response = requests.get(f"https://oauth.reddit.com/user/{user}/comments?limit={limit}&after={after}", headers=headers)
+    out = response.json()
+    after = out['data']['after']
+    comments = out['data']['children']
+    for comment in comments:
+        if comment['data']['subreddit'] == subreddit and comment['data']['parent_id'] == comment['data']['link_id']:
+            count += 1
+            filename = f'raw_stories/{count}.txt'
+            with open(filename, 'w') as file:
+                file.write(comment['data']['link_title'])
+                file.write('\n')
+                file.write(f"{comment['data']['created_utc']}")
+                file.write('\n-----\n')
+                file.write(comment['data']['body'])
+    # Record the number of comments returned from this request. 
+    # Once that number drops below the max number that will be returned, the loop will stop.
+    num_comments = len(comments)
