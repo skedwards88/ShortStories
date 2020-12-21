c81daa7a37b51c9bf22eb90f8ea87e9a897c4bc7
skedwards88 <skedwards88@github.com>
2020-12-21 12:55:51 -0800

    WIP

--- story-data/compile-stories.js
+++ story-data/compile-stories.js
@@ -2,3 +2,10 @@
 // for file in folder:
 //   make a chile div
 //   put the contents of the file into the div
+
+var http = require('http');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end('Hello World!');
+}).listen(8085);
