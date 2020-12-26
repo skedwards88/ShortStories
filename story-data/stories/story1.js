b8fa054e78d46858bf05e67897f7c7e1b20d2a9c
skedwards88 <skedwards88@github.com>
2020-12-26 13:42:43 -0800

    delete old files

--- story-data/stories/story1.js
+++
@@ -1,9 +0,0 @@
-import Story from '../story.js'
-
-export const story = new Story({
-    title: "pig houses",
-    text: "Talking pigs building houses. Wolf storm. Huff Puff. Brick wins.",
-    author: "Colin Thom",
-    date: "2020-11-02", // time stamps??
-    tags: ["scifi"],
-})
--- story-data/stories/story2.js
+++
@@ -1,7 +0,0 @@
-export const story = new Story({
-    title: "riding hood",
-    text: "gullible girl delivers sweets. Wolf gets dinner. Yum!",
-    author: "Colin Thom",
-    date: "2020-11-02", // time stamps??
-    tags: ["fantasy", "humor"],
-})
