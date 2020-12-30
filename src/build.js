3803471fb6b0ecd832736427f70abbe329921f26
skedwards88 <skedwards88@github.com>
2020-12-30 07:53:10 -0800

    cleanup

--- src/build.js
+++ src/build.js
@@ -5,16 +5,21 @@ const fm = require("front-matter");
 const marked = require("marked");
 
 function parseStory(storyPath) {
+    // Read the story file
     const data = fs.readFileSync(`${config.dev.contentDir}/${storyPath}`, "utf8");
+
+    // Process the front matter and get the body
     const content = fm(data);
+
+    // Mark up the body (HTML)
     content.body = marked(content.body);
-    content.path = storyPath;
+    
     return content;
 };
 
 // Read all the stories from the content directory, 
 // and generate an object for each story containing 
-// the front matter data and the HTML-marked up story
+// the front matter data (attributes) and the HTML-marked up story (body)
 const stories = fs
     .readdirSync(config.dev.contentDir)
     .map(story => parseStory(story))
